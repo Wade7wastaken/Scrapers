1accd0d669500be4f8ae50a09d10886bec807641
David C <david@thecallenders.com>
2023-08-28 21:27:08 -0500

    add doodlesFetcher

---
+++ doodlesFetcher.js
@@ -0,0 +1,41 @@
+import { load } from "cheerio";
+
+const getPageContent = async (url) => {
+	const page = await fetch(url);
+	return await page.text();
+};
+
+const getPropertyFromString = (string, name) => {
+	const beginningIndex = string.indexOf(`"${name}":`);
+
+	return JSON.parse(
+		`{${string.slice(beginningIndex, string.indexOf(",", beginningIndex))}}`
+	)[name];
+};
+
+(async () => {
+	const response = await fetch(
+		"https://www.google.com/doodles/json/2023/7?hl=en"
+	);
+	const json = await response.json();
+
+	json.forEach(async (element) => {
+		console.log("async started");
+		const doodleUrl = `https://www.google.com/doodles/${element.name}`;
+
+		const $ = load(await getPageContent(doodleUrl));
+
+		const scriptData = $("script").last().html();
+
+		if (scriptData === null) {
+			throw new Error("Couldn't find script");
+		}
+
+		const finalUrl = getPropertyFromString(scriptData, "standalone_html");
+
+		if (finalUrl.length !== 0) {
+			console.log(finalUrl);
+			console.log(getPropertyFromString(scriptData, "title"));
+		}
+	});
+})();
