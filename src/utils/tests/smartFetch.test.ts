47322efb00fa2c75848880b246038a447b8f24c7
Wade7wastaken <david@thecallenders.com>
2023-10-20 12:38:57 -0500

    formatting

--- src/utils/tests/smartFetch.test.ts
+++ src/utils/tests/smartFetch.test.ts
@@ -5,6 +5,8 @@ import { getDomain } from "../smartFetch.js";
 describe("fetch wrappers that delay between requests", () => {
 	it("gets the domain name from a url", () => {
 		expect(getDomain("https://www.google.com")).toBe("google");
-		expect(getDomain("https://abcdefg.domain.ending?search=params")).toBe("domain");
+		expect(getDomain("https://abcdefg.domain.ending?search=params")).toBe(
+			"domain"
+		);
 	});
 });
