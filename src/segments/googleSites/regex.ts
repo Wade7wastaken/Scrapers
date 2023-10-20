39016061928bca46af6601f99668d4218212623c
Wade7wastaken <david@thecallenders.com>
2023-10-20 12:45:25 -0500

    rename

--- src/segments/googleSites/regex.ts
+++ src/segments/googleSites/regex.ts
@@ -2,9 +2,9 @@ const combine = (a: RegExp, b: RegExp): RegExp =>
 	new RegExp(String(a).slice(1, -1) + String(b).slice(1, -1));
 
 export const url =
-	/^https?:\/\/(www\.)?[\w#%+.:=@~-]{1,256}\.[\d()A-Za-z]{1,6}\b([\w#%&()+./:=?@~-]*)/;
+	/^https?:\/\/(www\.)?[\w#%+.:=@~-]{1,256}\.[\d()A-Za-z]{1,24}\b([\w#%&()+./:=?@~-]*)/;
 
-export const stopAtQuot = combine(url, /(?=&quot)/);
+export const number = /^\d+/;
 
 export const alphaNumeric = /^\w*/;
 
