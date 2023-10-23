fc22afdde62f03e848a7b3b8ef80fbcc00f4b45e
Wade7wastaken <david@thecallenders.com>
2023-10-23 17:36:34 -0500

    begin work on tests for google Sites

---
+++ src/segments/googleSites/tests/processDataCode.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from "vitest";
+import { processDataCode } from "../processDataCode.js";
+import { TestLogger } from "../../../utils/logger.js";
+
+describe("tests an embed against test cases", () => {
+    it("tests a given data-code embed", () => {
+        const log = new TestLogger();
+
+        
+    })
+})
