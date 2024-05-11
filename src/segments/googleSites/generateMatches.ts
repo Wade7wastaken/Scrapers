d0ea66c099e6c7d4e8f766a4c95627a6112775f5
David C <david@thecallenders.com>
2024-05-11 08:00:49 -0500

    begin working on new match system

---
+++ src/segments/googleSites/generateMatches.ts
@@ -0,0 +1,36 @@
+import { readdir, stat, readFile } from "node:fs/promises";
+import { join } from "node:path";
+
+import { asyncIterator } from "@segments/asyncIterator";
+
+const MATCHES_DIR = "./src/segments/googleSites/matches";
+
+const generateMatches = async () => {
+	const files = await readdir(MATCHES_DIR);
+	await asyncIterator(files, async (file) => {
+		const fullPath = join(MATCHES_DIR, file);
+		const info = await stat(fullPath);
+		if (info.isFile()) {
+			const contents = await readFile(fullPath, { encoding: "utf8" });
+			parseMatch(contents, file);
+		}
+	});
+};
+
+type Match = {
+	name: string;
+	segments: (string | { regex: RegExp; included: boolean })[];
+};
+
+const parseMatch = (match: string, filename: string): Match => {
+	const segments = match.split("\r\n").map((line) => {
+		return line.startsWith("@")
+			? { regex: / /, included: line[1] == "+" }
+			: line;
+	});
+
+	return {
+		name: filename,
+		segments,
+	};
+};
