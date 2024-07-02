b5ac233c3cd3aba9c2803deeea8e0bd19fbf9e83
David C <david@thecallenders.com>
2024-07-02 16:59:02 -0500

    much better smarterFetch

--- src/utils/smarterFetch.ts
+++ src/utils/smarterFetch.ts
@@ -1,10 +1,59 @@
-import axios from "axios";
+import axios, { isAxiosError, type AxiosRequestConfig } from "axios";
+import axiosRetry, { isNetworkOrIdempotentRequestError } from "axios-retry";
+import { ResultAsync, err, ok } from "neverthrow";
 
-import { sleep } from "./misc";
+import { capitalize, sleep, smartInspect } from "./misc";
 
-import { REQUEST_DELAY_MS, REQUEST_DELAY_WAIT_MULTIPLIER } from "@config";
+import type { AxiosError } from "axios";
+import type { Result } from "neverthrow";
+import type { ZodError, ZodSchema, z } from "zod";
 
+import {
+	MAX_RETRIES,
+	REQUEST_DELAY_MS,
+	REQUEST_DELAY_WAIT_MULTIPLIER,
+} from "@config";
+
+// how long to wait given how many retries there have been
+export const getRetryMS = (retries: number): number =>
+	2 ** (retries + 1) * 1000;
+
+// formats a config into a string used for errors and warnings
+// might not be a bad idea to memo this because smartInspect can be expensive
+const formatError = (config: AxiosRequestConfig): string =>
+	`request to ${config.url} failed with params ${smartInspect(config.params)}`;
+
+// create an axios instance
 const instance = axios.create();
+
+// config for 3rd party axios-retry package
+axiosRetry(instance, {
+	retries: MAX_RETRIES,
+	retryDelay: getRetryMS,
+	onRetry(retryCount, _error, requestConfig) {
+		requestConfig.ctx.warn(
+			`Retriable error on request to ${formatError(requestConfig)}. Retrying in ${getRetryMS(retryCount)}ms. Error details:`
+		);
+	},
+	onMaxRetryTimesExceeded(error, retryCount) {
+		error.config?.ctx.error(
+			`${capitalize(formatError(error.config))} failed after ${retryCount} attempts`
+		);
+	},
+	retryCondition(error) {
+		const shouldRetry = isNetworkOrIdempotentRequestError(error);
+		if (shouldRetry)
+			error.config?.ctx.warn(
+				`Retriable error on request to ${formatError(error.config)}.`
+			);
+		else
+			error.config?.ctx.error(
+				`Unretriable error on request to ${formatError(error.config)}: ${error.response?.status}`
+			);
+
+		return shouldRetry;
+	},
+});
 
 // a mapping between domains and if they're ready for another request
 const domains = new Map<string, boolean>();
@@ -16,7 +65,10 @@ export const getDomain = (url: string): string => {
 	return noEnding.slice(noEnding.lastIndexOf(".") + 1);
 };
 
-const waitForNetwork = async (url: string): Promise<void> => {
+// throttles based on the request's domain. Requests that have the same domain
+// must bet at least REQUEST_DELAY_MS apart, but request from different domains
+// can be instant.
+const throttleDomain = async (url: string): Promise<void> => {
 	const domain = getDomain(url);
 
 	// make sure the domain exists in the map
@@ -34,7 +86,36 @@ const waitForNetwork = async (url: string): Promise<void> => {
 
 instance.interceptors.request.use(async (config) => {
 	const url = config.url ?? "";
-	config.ctx.info(`Request started: ${url}`)
-	await waitForNetwork(url);
+	config.ctx.info(`Request started: ${url}`);
+	await throttleDomain(url);
 	return config;
 });
+
+instance.interceptors.response.use((response) => {
+	response.config.ctx.info(`Request to ${response.config.url} succeeded`);
+	return response;
+});
+
+// still not sure what this eslint error means
+// eslint-disable-next-line @typescript-eslint/unbound-method
+export const axiosGetSafe = ResultAsync.fromThrowable(instance.get, (err) => {
+	if (isAxiosError(err)) return err;
+	else throw new Error(`Unknown error received from axios: ${String(err)}`);
+});
+
+const safeParseResult = <T extends ZodSchema>(
+	expectedType: T,
+	data: unknown
+): Result<z.infer<T>, ZodError> => {
+	const parseResult = expectedType.safeParse(data);
+	return parseResult.success ? ok(parseResult.data) : err(parseResult.error);
+};
+
+export const fetchAndParse = <T extends ZodSchema>(
+	url: string,
+	options: AxiosRequestConfig,
+	expectedType: T
+): ResultAsync<z.infer<T>, AxiosError | ZodError> =>
+	axiosGetSafe<unknown>(url, options).andThen((response) =>
+		safeParseResult(expectedType, response.data)
+	);
