815e2787bea385b6feb6e78916857d4ef3c8539a
David C <david@thecallenders.com>
2024-07-03 01:35:09 -0500

    small refactor

--- src/segments/fetchAndParse.ts
+++ src/segments/fetchAndParse.ts
@@ -1,17 +1,14 @@
-import { Ok, type Result } from "@thames/monads";
 import { load } from "cheerio";
 import { z } from "zod";
 
 import type { Context } from "@utils/context";
 import type { CheerioAPI } from "cheerio";
+import type { ResultAsync } from "neverthrow";
 
-import { smartFetch } from "@utils/smartFetch";
+import { fetchAndParse } from "@utils/smarterFetch";
 
-export const fetchAndParseHTML = async (
+export const fetchAndParseHTML = (
 	ctx: Context,
 	url: string
-): Promise<Result<CheerioAPI, string>> => {
-	const fetchResult = await smartFetch(ctx, url, z.string());
-	// im pretty sure cheerio.load doesn't throw anything
-	return fetchResult.andThen((page) => Ok(load(page)));
-};
+): ResultAsync<CheerioAPI, string> =>
+	fetchAndParse(ctx, url, z.string()).map((str) => load(str));
